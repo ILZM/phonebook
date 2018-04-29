f12765ed524d1b43233aa445343bafe75a8c6241
ILZM <nurmurat19@gmail.com>
2018-04-29 18:12:15 +0600

    Написан сервис

--- src/main/java/com/example/demo/service/real/PhonebookServiceReal.java
+++ src/main/java/com/example/demo/service/real/PhonebookServiceReal.java
@@ -1,16 +1,36 @@
 package com.example.demo.service.real;
 
+import com.example.demo.Util;
+import com.example.demo.domain.PhonebookDomain;
+import com.example.demo.mapper.PhonebookMapper;
 import com.example.demo.model.PhonebookRecord;
 import com.example.demo.service.PhonebookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PhonebookServiceReal implements PhonebookService {
 
+  @Autowired
+  private PhonebookMapper phonebookMapper;
+
   @Override
   public List<PhonebookRecord> getList() {
-    return null;
+    List<PhonebookDomain> domainList = phonebookMapper.getList();
+
+    List<PhonebookRecord> recordList = new ArrayList<>();
+    for (PhonebookDomain domain : domainList) {
+      PhonebookRecord record = new PhonebookRecord();
+      record.fullname = Util.getFullname(domain.firstname, domain.middlename, domain.lastname);
+      record.phoneNumber = domain.phoneNumber;
+      record.email = domain.email;
+
+      recordList.add(record);
+    }
+
+    return recordList;
   }
 }
